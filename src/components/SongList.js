8c3692bbe9e84aab764f2aa2587719fcb83aa61e
David Bronne <davidbronne@gmail.com>
2021-05-30 10:46:39 +0200

    useState

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React , {useState} from 'react';
+/* import uuid from 'uuid/v1'; */
+
 
 const SongList = () => {
+
+    const [songs, setSongs] = useState([
+        {id:1 , title:'Song1'},
+        {id:2 , title:'Song2'},
+        {id:3 , title:'Song3'}
+    ])
+
+    const addSong = () => {
+        setSongs([...songs,
+        {id: Math.random() , title:'new Song'}])
+    }
+
     return (
         <div className="song-list">
-            <ul>
-                <li>Song 1</li>
-                <li>Song 2</li>
+            <ul>{
+                songs.map( song => {
+                return( 
+                    <div>
+                        <li key={song.id} >{song.title}</li>
+                    </div>
+                    )
+                })}
             </ul>
+            <button onClick={addSong}>Add</button>
         </div>
     );
 }
